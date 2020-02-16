224a18760e1babe11df913c101f69312af185adf
thufir <hawat.thufir@gmail.com>
2020-02-16 00:00:53 -0800

    incremental

--- csv/foo.ps1
+++ csv/foo.ps1
@@ -1,34 +1,23 @@
-$text = Get-Content -Raw ./people.csv
+$list = Get-Content -Raw ./people.csv
 [array]::Reverse($list)
 
-
-class OnePerson {
-  [string] $name
-  [string[]] $Attribute
-}
-
 $tempAttributes = @()
-
 $collectionOfPeople = @()
 
-
-$tempPerson = [OnePerson]::new()
-
-
-
 ForEach ($line in $list) {
-  $tempAttributes += $line    
   if ($line -match '[0-9]') {
-    write-host "---------------"
-  }
-  else {}
+    write-host "attribute" 
+    $line
+    $tempAttributes += $line    
 
-$records = $text -split '\r?\n(?=[a-z '-]+\r?\n)'
-foreach ($person in $records) {
-    $details = $person -split '\r?\n'
-    
-    [PSCustomObject]@{
-        Name = $details[0]
-        # Fill in other details here from the rest of the pieces
+  }
+  else {
+
+    $foo = [PSCustomObject]@{
+      Name        = $details[0]
+      $Attributes = $tempAttributes
     }
+    $tempAttributes = @()
+  }
 }
+
