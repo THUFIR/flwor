2eb684bce5fc38cd4020eb2b2669508423363cbd
thufir <hawat.thufir@gmail.com>
2020-02-16 03:51:34 -0800

    incremental

--- csv/import.ps1
+++ csv/import.ps1
@@ -1,23 +1,5 @@
-$text = Get-Content -Raw ./people.csv
-[array]::Reverse($text)
 
-$tempAttributes = @()
-$collectionOfPeople = @()
-
-ForEach ($line in $text) { 
-  if($line -notmatch '.*?[0-9].*?') {
-    $tempAttributes += $line 
-    Write-Host "matches digits"   
-  }
-  else {
-    Write-Host "no digits"   
-    $newPerson = [PSCustomObject]@{
-      Name       = $line
-      Attributes = $tempAttributes
-    }
-    $tempAttributes = @()
-    $collectionOfPeople += $newPerson
-  }
+switch -regex -file people.csv {
+  '\d' { "yes" ; $_ }
+  default { "no"; $_ }
 }
-
-$collectionOfPeople
