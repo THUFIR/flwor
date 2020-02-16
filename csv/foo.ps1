9e21486a10fbf289a383d1141cd07adc08e619e8
thufir <hawat.thufir@gmail.com>
2020-02-15 23:55:05 -0800

    incremental

--- csv/foo.ps1
+++ csv/foo.ps1
@@ -1,4 +1,27 @@
 $text = Get-Content -Raw ./people.csv
+[array]::Reverse($list)
+
+
+class OnePerson {
+  [string] $name
+  [string[]] $Attribute
+}
+
+$tempAttributes = @()
+
+$collectionOfPeople = @()
+
+
+$tempPerson = [OnePerson]::new()
+
+
+
+ForEach ($line in $list) {
+  $tempAttributes += $line    
+  if ($line -match '[0-9]') {
+    write-host "---------------"
+  }
+  else {}
 
 $records = $text -split '\r?\n(?=[a-z '-]+\r?\n)'
 foreach ($person in $records) {
